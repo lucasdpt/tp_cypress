2f41acfff175e361fcc6d967ee967c001457f261
dptgwennaelle <depoorter.gwenaelle@gmail.com>
2025-06-17 11:23:42 +0200

    Ajout test consultation page d'accueil

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,10 @@
+describe('Page d\'accueil', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3030');
+    });
+
+    it('Affiche la page d\'accueil', () => {
+        cy.contains('Ajouter').should('exist');
+    });
+
+});
