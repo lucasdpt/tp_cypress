b567577421ff57498d23e2e32a7184c4be9fe026
lucasdpt <github@zertus.fr>
2025-06-17 12:05:36 +0200

    Fix bug

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -27,11 +27,6 @@ export default class TodoApp extends Component {
   }
   
   handleNewTodoChange(evt) {
-	// Check if input value already exists in the todos
-	if (this.state.todos.some(todo => todo.name === evt.target.value)) {
-		this.setState({error: true})
-		return
-	}
 	  this.setState({currentTodo: evt.target.value})
   }
   
