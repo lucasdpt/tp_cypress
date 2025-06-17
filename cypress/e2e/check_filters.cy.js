a46e4ff76073f87d3c610611fac89399e0faa21a
dptgwennaelle <depoorter.gwenaelle@gmail.com>
2025-06-17 12:18:57 +0200

    Check filters

---
+++ cypress/e2e/check_filters.cy.js
@@ -0,0 +1,34 @@
+describe('Check filter task\'s', () => {
+    const task1 = 'Sleep a loooot'
+    const task2 = 'Eat sushi'
+
+    beforeEach(() => {
+        cy.visit('http://localhost:3030')
+
+        cy.get('.new-todo').type(`${task1}{enter}`)
+        cy.get('.new-todo').type(`${task2}{enter}`)
+        cy.contains(task2)
+            .parents('li')
+            .find('input[type="checkbox"]')
+            .check({ force: true })
+        cy.contains('Tous').click()
+    })
+
+    it('Filter : All / "Tous" ', () => {
+        cy.contains('Tous').click()
+        cy.contains(task1).should('exist')
+        cy.contains(task2).should('exist')
+    })
+
+    it('Filter : not completed / "En cours" ', () => {
+        cy.contains('Fait').click()
+        cy.contains(task1).should('not.exist')
+        cy.contains(task2).should('exist')
+    })
+
+    it('Filter : completed / "Fait"', () => {
+        cy.contains('Fait').click()
+        cy.contains(task1).should('not.exist')
+        cy.contains(task2).should('exist')
+    })
+})
